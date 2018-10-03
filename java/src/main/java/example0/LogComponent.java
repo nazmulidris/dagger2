a221143ff317847e2910d2070c345708d36708a9
Nazmul Idris <nazmul@fasterlap.com>
2018-10-02 18:32:28 -0700

    Add simplest example for using Dagger 2 (example0)

---
+++ java/src/main/java/example0/LogComponent.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 Nazmul Idris. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example0;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
+
+@Component(modules = {LogModule.class})
+@Singleton
+interface LogComponent {
+    Main providesMain();
+}
+
+@Module
+class LogModule {
+    @Provides @Singleton
+    Log providesLogger(){return new ConsoleLogger();}
+}
